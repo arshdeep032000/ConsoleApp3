6d8562622f5384272c69e5f9d6b72b529e23ec80
visualstudio <visualstudio@122-PC22>
2019-02-27 10:05:22 -0500

    intiation

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,8 +8,38 @@ namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        var orderProcessor = new orderProcessor();
+        var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+        orderProcessor.Process(order);
+            }
+}
+public class orderProcessor
+{
+    private readonly ShippingCalculator_ShippingCalculator;
+                public orderProcessor()
+    {
+        _ShippingCalculator = new ShippingCalculator();
+    }
+    public void Process(Order order)
+    {
+        if (order.IsShipped)
+            throw new InvalidOperationException("this order is already processed");
+        order.Shipment = new Shipment
         {
-        }
+            Cost = _shippingCalculator.CalculateShipping(Order),
+            ShippingDate = DateTime.Today.AddDays(1);
     }
 }
+        }
+ public class ShippingCalculator
+{
+    public float CalculateShipping(Orderorder)
+    {
+        if (order.TotalPrice < 30f) return order.TotalPrice * 0.1f;
+        return0;
+    }
+}  
+        
+        
+    
+
---
+++ c0730385unittests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("c0730385unittests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("c0730385unittests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d55a7f95-48e5-4d14-b7b3-238fb7fe2c6e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
